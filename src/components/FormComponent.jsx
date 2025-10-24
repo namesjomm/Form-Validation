8b668ec472cdaa0219f0557b2cefc466f2e1078f
Gontee <nmsjm.gontee@gmail.com>
2025-10-24 10:36:08 +0700

    Add validation password features

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -6,15 +6,56 @@ export default function FormComponent() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const [errorUserName, setErrorUserName] = useState("Username must be between 3 and 25 characters.");
-  const [errorEmail, setErrorEmail] = useState("Email is invalid");
-  const [errorPassword, setErrorPassword] = useState('Password has 8 characters or longer. And it must contain 1 lowercase character, 1 uppercase character, 1 number, and at least 1 special character in this set (!@#$%^&*).');
-  const [errorConfirmPassword, setErrorConfirmPassword] = useState("Please enter the password again.");
+  const [errorUserName, setErrorUserName] = useState();
+  const [errorEmail, setErrorEmail] = useState();
+  const [errorPassword, setErrorPassword] = useState();
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState();
+
+  const [userNameColor, setUserNameColor] = useState("");
+  const [emailColor, setEmailColor] = useState("");
+  const [passwordColor, setPasswordColor] = useState("");
+  const [confirmPasswordColor, setConfirmPasswordColor] = useState("");
+
+  const validateForm = (e) => {
+    e.preventDefault();
+
+    if (userName.length > 3) {
+      setErrorUserName("");
+      setUserNameColor("green");
+    } else {
+      setErrorUserName("Username must be between 3 and 25 characters.");
+      setUserNameColor("red");
+    }
+
+    if (email.includes("@")) {
+      setErrorEmail("");
+      setEmailColor("green");
+    } else {
+      setErrorEmail("Email is invalid");
+      setEmailColor("red");
+    }
+
+    if (password.length > 8) {
+      setErrorPassword("");
+      setPasswordColor("green");
+    } else {
+      setErrorPassword("Password has 8 characters or longer.");
+      setPasswordColor("red");
+    }
+
+    if (confirmPassword != "" && confirmPassword === password) {
+      setErrorConfirmPassword("");
+      setConfirmPasswordColor("green");
+    } else {
+      setErrorConfirmPassword("Please enter the password again.");
+      setConfirmPasswordColor("red");
+    }
+  };
 
   return (
     <>
       <div className="container">
-        <form className="form">
+        <form className="form" onSubmit={validateForm}>
           <h2>Sign Up</h2>
           <div className="form-control">
             <label>Username:</label>
@@ -22,8 +63,9 @@ export default function FormComponent() {
               type="text"
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
+              style={{ borderColor: userNameColor }}
             />
-            <small>{errorUserName}</small>
+            <small style={{ color: userNameColor }}>{errorUserName}</small>
           </div>
           <div className="form-control">
             <label>Email:</label>
@@ -31,26 +73,31 @@ export default function FormComponent() {
               type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              style={{ borderColor: emailColor }}
             />
-            <small>{errorEmail}</small>
+            <small style={{ color: emailColor }}>{errorEmail}</small>
           </div>
           <div className="form-control">
             <label>Password:</label>
             <input
-              type="text"
+              type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              style={{ borderColor: passwordColor }}
             />
-            <small>{errorPassword}</small>
+            <small style={{ color: passwordColor }}>{errorPassword}</small>
           </div>
           <div className="form-control">
             <label>Confirm Password:</label>
             <input
-              type="text"
+              type="password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
+              style={{ borderColor: confirmPasswordColor }}
             />
-            <small>{errorConfirmPassword}</small>
+            <small style={{ color: confirmPasswordColor }}>
+              {errorConfirmPassword}
+            </small>
           </div>
           <button type="submit">SIGN UP</button>
         </form>
